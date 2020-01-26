25f3e96e773d4698fd703a1fec83bb8737c55cd5
woonbeom <baek12201@gmail.com>
2020-01-26 17:19:04 +0900

    CreatingHabbit 컴포넌트 추가

--- react-habittracker/src/App.tsx
+++ react-habittracker/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import MainTemplate from './components/MainTemplate';
 import Form from './components/Form';
-import HabitList from './components/HabitList';
+// import HabitList from './components/HabitList';
+import CreatingHabbit from './components/CreatingHabbit';
 
 // const App: React.FC = () => {
 function App () {
   return (
     <MainTemplate form={<Form />}> 
-      <HabitList/>
+      {/* <HabitList/> */}
+    <CreatingHabbit/>      
     </MainTemplate>
   );
 }
---
+++ react-habittracker/src/components/CreatingHabbit.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './CreatingHabbit.css'
+
+function CreatingHabbit () {
+    return (
+        <div>
+            <div>
+                Title
+            </div>
+            <div>
+                <input></input>
+            </div>
+            <div>
+                Cycle
+            </div>
+            <div>
+                Once in <input></input> days
+            </div>
+            <div>
+                Action
+            </div>
+            <div>
+                <input></input>
+            </div>
+        </div>
+    );
+}
+
+export default CreatingHabbit;

5c4ae5e78643a22d8c6b20926d6bc597a04f0580
백운범 <woonbeom_baek@tmax.co.kr>
2020-01-27 17:45:02 +0900

    react-router 추가

--- react-habittracker/src/App.tsx
+++ react-habittracker/src/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import MainTemplate from './components/MainTemplate';
+
+import Main from './routes/Main';
+import Create_1 from './routes/Create_1';
 import Form from './components/Form';
-// import HabitList from './components/HabitList';
-import CreatingHabbit from './components/CreatingHabbit';
 
 // const App: React.FC = () => {
 function App () {
   return (
-    <MainTemplate form={<Form />}> 
-      {/* <HabitList/> */}
-    <CreatingHabbit/>      
-    </MainTemplate>
+    <Router>
+      <MainTemplate form={<Form />} onCreate={()=>{}}> 
+        <Switch>
+          <Route exact path="/" component={Main}/>
+          <Route path="/create_1" component={Create_1}/>
+        </Switch>
+      </MainTemplate>
+    </Router>
   );
 }
 
---
+++ react-habittracker/src/routes/Create_1.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import CreatingHabbit_1 from '../components/CreatingHabbit_1';
+
+function Create_1 () {
+    return (
+        <CreatingHabbit_1/>
+    );
+}
+
+export default Create_1;

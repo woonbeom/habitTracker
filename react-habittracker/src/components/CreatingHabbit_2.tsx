7a9455beb99b95961d537f9483c49016a06d83ef
백운범 <woonbeom_baek@tmax.co.kr>
2020-01-27 20:50:12 +0900

    antd date picker료 대체

--- react-habittracker/src/components/CreatingHabbit_2.tsx
+++ react-habittracker/src/components/CreatingHabbit_2.tsx
@@ -1,60 +1,17 @@
 import React, { Component } from 'react';
-import DatePicker from 'react-datepicker';
+import { DatePicker } from 'antd';
+import 'antd/dist/antd.css'
 
-import "react-datepicker/dist/react-datepicker.css"
+const { RangePicker } = DatePicker;
 
-class CreatingHabbit_2 extends Component {
-    state = {
-        startDate: new Date(),
-        endDate: new Date()
-    };
+function onChange(date, dateString) {
+    console.log(date, dateString);
+}
 
-    handleStartDateChange = date => {
-        this.setState({
-            startDate: date
-        });
-    }
-
-    handleStartDateSelect = date => {
-        this.setState({
-            startDate: date
-        });
-    }
-
-    handleEndDateChange = date => {
-        this.setState({
-            endDate: date
-        });
-    }
-
-    handleEndDateSelect = date => {
-        this.setState({
-            endDate: date
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <div>
-                    From
-                </div>
-                <DatePicker
-                    selected={this.state.startDate}
-                    onChagne={this.handleStartDateChange}
-                    onSelect={this.handleStartDateSelect}
-                />
-                <div>
-                    To
-                </div>
-                <DatePicker
-                    selected={this.state.endDate}
-                    onChagne={this.handleEndDateChange}
-                    onSelect={this.handleEndDateSelect}
-                />
-            </div>
-        );
-    }
+function CreatingHabbit_2 () {
+    return (
+        <RangePicker onChange={onChange}/>
+    );
 }
 
 export default CreatingHabbit_2;

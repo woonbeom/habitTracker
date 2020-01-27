cad8fee4977bf4dedd7c25e90f72f67d6d1e8fba
woonbeom <baek12201@gmail.com>
2020-01-28 07:33:03 +0900

    antd checkbox, timepicker 적용

--- react-habittracker/src/components/CreatingHabbit_3.tsx
+++ react-habittracker/src/components/CreatingHabbit_3.tsx
@@ -1,6 +1,17 @@
 
 import React from 'react';
+import { Input, TimePicker, Checkbox } from 'antd';
 import './CreatingHabbit_3.css'
+import 'antd/dist/antd.css'
+import moment from 'moment';
+
+function onTimePickerChange(time, timeString) {
+    console.log(time, timeString);
+}
+
+function onCheckboxChange(e) {
+    console.log(`checked = ${e.target.checked}`);
+}
 
 function CreatingHabbit_3 () {
     return (
@@ -9,16 +20,22 @@ function CreatingHabbit_3 () {
                 How to log 
             </div>
             <div>
-                Q.<input></input>
+                Q.<Input placeholder="How to log"/> 
             </div>
             <div>
                 When to log
             </div>
             <div>
-                Pick Week days
+                <Checkbox onChange={onCheckboxChange}>Mon</Checkbox>
+                <Checkbox onChange={onCheckboxChange}>Tue</Checkbox>
+                <Checkbox onChange={onCheckboxChange}>Wed</Checkbox>
+                <Checkbox onChange={onCheckboxChange}>Thu</Checkbox>
+                <Checkbox onChange={onCheckboxChange}>Fri</Checkbox>
+                <Checkbox onChange={onCheckboxChange}>Sat</Checkbox>
+                <Checkbox onChange={onCheckboxChange}>Sun</Checkbox>
             </div>
             <div>
-                Time
+                <TimePicker onChange={onTimePickerChange} defaultOpenValue={moment('00:00:00', 'HH:mm:ss')} />
             </div>
         </div>
     );

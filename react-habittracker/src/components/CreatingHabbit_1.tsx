6d4a27d1071edfa52ea0588dd39e0e0b0ce4a558
woonbeom <baek12201@gmail.com>
2020-02-01 20:48:26 +0900

    CreatingHabbit_1 컴포넌트 useState 적용

--- react-habittracker/src/components/CreatingHabbit_1.tsx
+++ react-habittracker/src/components/CreatingHabbit_1.tsx
@@ -1,29 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Input, Button } from 'antd';
 import './CreatingHabbit_1.css'
 import 'antd/dist/antd.css'
 
 function CreatingHabbit_1() {
+    const [title, setTitle] = useState("");
+    const [cycle, setOnceEveryNDays] = useState("");
+    const [action, setAction] = useState("");
+
+    useEffect(() => {
+        console.log("렌더링 완료!");
+        console.log({
+            title,
+            cycle,
+            action
+        });
+    }, [title, cycle, action]);
+
+    const onChangeTitle = (e) => {
+        setTitle(e.target.value);
+    };
+
+    const onChangeCycle = (e) => {
+        setOnceEveryNDays(e.target.value);
+    };
+
+    const onChangeAction = (e) => {
+        setAction(e.target.value);
+    };
+
     return (
         <div>
             <div>
                 Title
             </div>
             <div>
-                <Input placeholder="Title" className="long-input"/>
+                <Input className="long-input" value={title} onChange={onChangeTitle} placeholder="Title"/>
             </div>
             <div>
                 Cycle
             </div>
             <div>
-                Once in <Input placeholder="7"/> days
+                Once in <Input value={cycle} onChange={onChangeCycle} placeholder="7"/> days
             </div>
             <div>
                 Action
             </div>
             <div>
-                <Input placeholder="Action" className="long-input"/>
+                <Input className="long-input" value={action} onChange={onChangeAction} placeholder="Action" />
             </div>
             <div>
                 <Link to="/">

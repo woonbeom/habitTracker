1ca8af6568c33298000f2b23f35f41904d4928cb
woonbeom <baek12201@gmail.com>
2020-02-01 20:49:36 +0900

    CreatingHabbit_2 컴포넌트 useState 적용

--- react-habittracker/src/components/CreatingHabbit_2.tsx
+++ react-habittracker/src/components/CreatingHabbit_2.tsx
@@ -1,19 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { DatePicker, Button } from 'antd';
 import 'antd/dist/antd.css'
 
 const { RangePicker } = DatePicker;
 
-function onChange(date, dateString) {
-    console.log(date, dateString);
-}
-
 function CreatingHabbit_2 () {
+    const [from, setFromDate] = useState("");
+    const [to, setToDate] = useState("");
+
+    const onChangeDate = (date, dateString) => {
+        setFromDate(dateString[0]);
+        setToDate(dateString[1]);
+        console.log(`fromDate:${from}`);
+        console.log(`toDate:${to}`);
+    }
+
     return (
         <div>
             <div>
-                <RangePicker onChange={onChange}/>
+                <RangePicker onChange={onChangeDate}/>
             </div>
             <div>
                 <Link to="create_1">

d37647ba08beab86bbc97761bd0681c6529f964e
woonbeom <baek12201@gmail.com>
2020-02-02 11:21:56 +0900

    CreatingHabbit_3 컴포넌트 useState 적용
    - 요일 체크박스 한 그룹으로 묶음.

--- react-habittracker/src/components/CreatingHabbit_3.tsx
+++ react-habittracker/src/components/CreatingHabbit_3.tsx
@@ -1,42 +1,71 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Input, TimePicker, Checkbox, Button } from 'antd';
+import { Input, TimePicker, Checkbox, Row, Col, Button } from 'antd';
 import './CreatingHabbit_3.css'
 import 'antd/dist/antd.css'
 import moment from 'moment';
 
-function onTimePickerChange(time, timeString) {
-    console.log(time, timeString);
-}
-
-function onCheckboxChange(e) {
-    console.log(`checked = ${e.target.checked}`);
-}
-
 function CreatingHabbit_3 () {
+    const [question, setQuestion] = useState('');
+    const [weekdays, setWeekdays] = useState([]);
+    const [time, setTime] = useState('');
+
+    const onChangeQuestion = (e) => {
+        setQuestion(e.target.value);
+        // console.log(`question: ${question}`);
+    }
+
+    const onChangeWeekdays = (checkedValues) => {
+        setWeekdays(checkedValues);
+        console.log(`checked Weekday: ${checkedValues}`);
+    };
+
+    const onChangeTime = (timeString) => {
+        setTime(timeString);
+        console.log(`time: ${time}`);
+    }
+
     return (
         <div>
             <div>
                 How to log 
             </div>
             <div>
-                Q.<Input placeholder="How to log"/> 
+                Q.<Input placeholder="How to log" onChange={onChangeQuestion}/> 
             </div>
             <div>
                 When to log
             </div>
             <div>
-                <Checkbox onChange={onCheckboxChange}>Mon</Checkbox>
-                <Checkbox onChange={onCheckboxChange}>Tue</Checkbox>
-                <Checkbox onChange={onCheckboxChange}>Wed</Checkbox>
-                <Checkbox onChange={onCheckboxChange}>Thu</Checkbox>
-                <Checkbox onChange={onCheckboxChange}>Fri</Checkbox>
-                <Checkbox onChange={onCheckboxChange}>Sat</Checkbox>
-                <Checkbox onChange={onCheckboxChange}>Sun</Checkbox>
-            </div>
+                <Checkbox.Group style={{ width: '100%' }} onChange={onChangeWeekdays}>
+                    <Row>
+                        <Col span={3}>
+                            <Checkbox value='Mon'>Mon</Checkbox>
+                        </Col>
+                        <Col span={3}>
+                            <Checkbox value='Tue'>Tue</Checkbox>
+                        </Col>
+                        <Col span={3}>
+                            <Checkbox value='Wed'>Wed</Checkbox>
+                        </Col>
+                        <Col span={3}>
+                            <Checkbox value='Thu'>Thu</Checkbox>
+                        </Col>
+                        <Col span={3}>
+                            <Checkbox value='Fri'>Fri</Checkbox>
+                        </Col>
+                        <Col span={3}>
+                            <Checkbox value='Sat'>Sat</Checkbox>
+                        </Col>
+                        <Col span={3}>
+                            <Checkbox value='Sun'>Sun</Checkbox>
+                        </Col>
+                    </Row>
+                </Checkbox.Group>
+           </div>
             <div>
-                <TimePicker onChange={onTimePickerChange} defaultOpenValue={moment('00:00:00', 'HH:mm:ss')} />
+                <TimePicker onChange={onChangeTime} defaultOpenValue={moment('00:00:00', 'HH:mm:ss')} />
             </div>
             <div>
                 <Link to="create_2">

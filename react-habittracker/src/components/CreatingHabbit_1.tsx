da91e393e26b47ed948efc016a384f89cae68e1a
woonbeom <baek12201@gmail.com>
2020-02-02 11:36:39 +0900

    CreatingHabbit_1 컴포넌트 내용 세션 스토리지에 저장하는 함수 추가

--- react-habittracker/src/components/CreatingHabbit_1.tsx
+++ react-habittracker/src/components/CreatingHabbit_1.tsx
@@ -5,29 +5,52 @@ import './CreatingHabbit_1.css'
 import 'antd/dist/antd.css'
 
 function CreatingHabbit_1() {
+    const initHabbitObject = (() => {
+        const habbitObject = {
+            title: "",
+            description: "",
+            cycle: {from: "", to: "", onceEveryNDays: ""},
+            action: {title: ""},
+            logging: {questionnaire: [], alarms: []},
+            reminder: []
+        }
+        sessionStorage.setItem('habbit', JSON.stringify(habbitObject));
+    })();
+
     const [title, setTitle] = useState("");
-    const [cycle, setOnceEveryNDays] = useState("");
+    const [onceEveryNDays, setOnceEveryNDays] = useState("");
     const [action, setAction] = useState("");
 
     useEffect(() => {
         console.log("렌더링 완료!");
         console.log({
             title,
-            cycle,
+            onceEveryNDays,
             action
         });
-    }, [title, cycle, action]);
+    }, [title, onceEveryNDays, action]);
 
     const onChangeTitle = (e) => {
         setTitle(e.target.value);
     };
 
-    const onChangeCycle = (e) => {
+    const onChangeOnceEveryNDays = (e) => {
         setOnceEveryNDays(e.target.value);
     };
 
     const onChangeAction = (e) => {
         setAction(e.target.value);
+    };
+
+    const onClickNextButton = () => {
+        if (sessionStorage.getItem('habbit')) {
+            let habbit = JSON.parse(sessionStorage.getItem('habbit') || '');
+            habbit.title = title;
+            habbit.cycle.onceEveryNDays = onceEveryNDays;
+            habbit.action.title = action;
+            sessionStorage.setItem('habbit', JSON.stringify(habbit));
+            console.log(habbit);
+        }
     };
 
     return (
@@ -36,13 +59,13 @@ function CreatingHabbit_1() {
                 Title
             </div>
             <div>
-                <Input className="long-input" value={title} onChange={onChangeTitle} placeholder="Title"/>
+                <Input className="long-input" value={title} onChange={onChangeTitle} placeholder="Title" />
             </div>
             <div>
                 Cycle
             </div>
             <div>
-                Once in <Input value={cycle} onChange={onChangeCycle} placeholder="7"/> days
+                Once in <Input value={onceEveryNDays} onChange={onChangeOnceEveryNDays} placeholder="7" /> days
             </div>
             <div>
                 Action
@@ -55,7 +78,7 @@ function CreatingHabbit_1() {
                     <Button size='large' className='create-button'>Back</Button>
                 </Link>
                 <Link to="/create_2">
-                    <Button size='large' className='create-button'>Next</Button>
+                    <Button size='large' className='create-button' onClick={onClickNextButton}>Next</Button>
                 </Link>
             </div>
         </div>
